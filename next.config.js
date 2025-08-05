57ace1b3505ec5ae7fcf0413477d3e810571c9e9
JuampaVLB <contact.juampavlb@gmail.com>
2025-08-05 14:58:31 -0600

    new file next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true, // Habilita el modo estricto de React
+  webpack(config) {
+    // Si necesitas alguna configuración personalizada para Webpack
+    return config;
+  },
+};
